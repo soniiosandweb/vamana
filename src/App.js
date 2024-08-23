be22ca18630a8462ddf00086d0e82539aeb45cfd
soni <soni@iosandweb.net>
2024-08-23 17:28:52 +0530

    cookie changes

--- src/App.js
+++ src/App.js
@@ -14,11 +14,12 @@ function App() {
         location="top"
         buttonText="Okay, Got It"
         cookieName="myAwesomeCookieName2"
-        style={{ background: "#fff", color: "#000", padding: "0 20px", top: "76px", alignItems: "center" }}
+        style={{ background: "#fff", color: "#000", padding: "0 20px", alignItems: "center", flexDirection: "column" }}
         buttonStyle={{ color: "#fff", fontSize: "16px", backgroundColor: "#C27C00", padding: "10px 30px" }}
         expires={150}
         overlay={true}
         containerClasses="cookie-banner"
+        contentStyle={{textAlign: "center", flex: 1}}
         overlayStyle={{backgroundColor: "#00000099"}}
       >
         'Vamana Residences' is only acting as a medium for providing online advertising services. 'Vamana Residences' does not in any way facilitate and cannot be deemed to be facilitating sales between developers and the visitors/users of the website. The display of information on 'Vamana Residences' with respect to a developer or project does not guarantee that the developer / project has registered under the Real Estate (Regulation and Development), 2016 or is compliant with the same. Before deciding to purchase or taking any other action, you are requested to exercise due caution and to independently validate and verify all information about the project.
