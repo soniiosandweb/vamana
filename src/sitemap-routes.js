042a31f428a7e4c844ac0a65cf7f790f73f19c59
shreyanshi <shreyanshi@iosandweb.net>
2024-10-07 15:17:33 +0530

    Latest Changes

--- src/sitemap-routes.js
+++ src/sitemap-routes.js
@@ -3,7 +3,14 @@ import { Route } from "react-router";
 
 export default (
     <Route>
-        <Route path='/privacy-policy'/>
-        <Route path='/terms-conditions'/>
+        <Route path='/privacy-policy' />
+        <Route path='/terms-conditions' />
+        <Route path='/floor-plan'  />
+        <Route path='/master-plan'  />
+        <Route path='/club-zaira'  />
+        <Route path='/location-map'  />
+        <Route path='/price-list' />
+        <Route path='/galleries'  />
+        <Route path='/amenitie'  />
     </Route>
 )
