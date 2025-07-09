fec1d1c0e42434283f926d526d2357b4e3ab92cc
iosandwebnet <soni@iosandweb.net>
2025-07-09 11:12:48 +0530

    SEO Helmet update

--- src/components/Layouts/Layout.jsx
+++ src/components/Layouts/Layout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header/header";
 import Footer from "./Footer/Footer";
+import { HelmetProvider } from "react-helmet-async";
 
 const Layout = () => {
     return(
         <>
-            <Header />
-            <Outlet />
-            <Footer />
+            <HelmetProvider>
+                <Header />
+                <Outlet />
+                <Footer />
+            </HelmetProvider>
         </>
         
     )
