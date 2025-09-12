e2f2cd0fe0181beebf41c4ca3f89ac031edbc577
iosandwebnet <soni@iosandweb.net>
2025-09-12 11:41:30 +0530

    update Meta

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
     return (
         <>
             <MetaData 
-                title={"Luxury 3 & 4bhk flats in Zirakpur - Vamana Arvindam Zirakpur"} 
-                description={"Discover luxury 3 & 4 BHK flats in Zirakpur. Explore new residential projects offering modern amenities, prime locations, and elegant living spaces for your dream home."} 
+                title={"Luxury 3bhk & 4bhk flats in Zirakpur - Vamana Arvindam Zirakpur"} 
+                description={"Discover luxury 3BHK & 4BHK flats in Zirakpur. Explore new residential projects offering modern amenities, prime locations, and elegant living spaces for your dream home."} 
                 keywords={"Vamana Group, Vamana Arvindam, 3 BHK flats Zirakpur, 3+1 BHK flats Zirakpur, 4+1 BHK flats Zirakpur, Flats near Patiala Road Zirakpur, Luxury apartments Zirakpur, Luxury Flats in Zirakpur, Premium Property in Zirakpur, New Residential Project in Zirakpur"}
                 canonicalUrl={process.env.REACT_APP_API_URL}
             />
